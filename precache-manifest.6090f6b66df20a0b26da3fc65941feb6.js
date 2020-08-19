094278a7be35bdef6681b8bc50210caae9e5eb4a
pandacode <the1ime@yandex.ru>
2020-08-19 16:15:02 +0500

    Updates

---
+++ precache-manifest.6090f6b66df20a0b26da3fc65941feb6.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7460448bbe4f861c49800b669250932",
+    "url": "/medicine-consultation-client/index.html"
+  },
+  {
+    "revision": "252c3e6693f5fa2f147b",
+    "url": "/medicine-consultation-client/static/css/2.cee80f42.chunk.css"
+  },
+  {
+    "revision": "c6df244f67f7cf9452ca",
+    "url": "/medicine-consultation-client/static/css/4.afdb2cb0.chunk.css"
+  },
+  {
+    "revision": "e72f98234e6c1d698c22",
+    "url": "/medicine-consultation-client/static/css/5.0d86f7eb.chunk.css"
+  },
+  {
+    "revision": "4686a2232b9355565182",
+    "url": "/medicine-consultation-client/static/css/9.a53ad590.chunk.css"
+  },
+  {
+    "revision": "252d5ffcb50662404114",
+    "url": "/medicine-consultation-client/static/css/main.7720bd7f.chunk.css"
+  },
+  {
+    "revision": "f6539fbf86314080abd5",
+    "url": "/medicine-consultation-client/static/js/0.fad99d75.chunk.js"
+  },
+  {
+    "revision": "e4a8ed607750ae35b2c4",
+    "url": "/medicine-consultation-client/static/js/1.e40e97f3.chunk.js"
+  },
+  {
+    "revision": "fe0d9a405704c41dcb6e",
+    "url": "/medicine-consultation-client/static/js/10.e6bce835.chunk.js"
+  },
+  {
+    "revision": "8bbb8872ae0fceef6daa",
+    "url": "/medicine-consultation-client/static/js/11.9dd58d65.chunk.js"
+  },
+  {
+    "revision": "252c3e6693f5fa2f147b",
+    "url": "/medicine-consultation-client/static/js/2.a6207562.chunk.js"
+  },
+  {
+    "revision": "6cba85ad3a79261ef8f3",
+    "url": "/medicine-consultation-client/static/js/3.05b883be.chunk.js"
+  },
+  {
+    "revision": "7141f10c40c9c06929b77dbfc46ec0d9",
+    "url": "/medicine-consultation-client/static/js/3.05b883be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6df244f67f7cf9452ca",
+    "url": "/medicine-consultation-client/static/js/4.dac4d0e6.chunk.js"
+  },
+  {
+    "revision": "e72f98234e6c1d698c22",
+    "url": "/medicine-consultation-client/static/js/5.0830b479.chunk.js"
+  },
+  {
+    "revision": "67b4d01af3822b6e0794",
+    "url": "/medicine-consultation-client/static/js/8.5f990707.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/medicine-consultation-client/static/js/8.5f990707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4686a2232b9355565182",
+    "url": "/medicine-consultation-client/static/js/9.6adbd439.chunk.js"
+  },
+  {
+    "revision": "252d5ffcb50662404114",
+    "url": "/medicine-consultation-client/static/js/main.cd532717.chunk.js"
+  },
+  {
+    "revision": "52cabea85b43425a4837",
+    "url": "/medicine-consultation-client/static/js/runtime-main.d37d4c8b.js"
+  },
+  {
+    "revision": "7a54497bc51fed1159f51131c36366d2",
+    "url": "/medicine-consultation-client/static/media/INTER-BOLD.7a54497b.TTF"
+  },
+  {
+    "revision": "89456b152302a9eab0206e17dcb92a4c",
+    "url": "/medicine-consultation-client/static/media/INTER-MEDIUM.89456b15.TTF"
+  },
+  {
+    "revision": "59e981f7c001d676710ae161ce5d8258",
+    "url": "/medicine-consultation-client/static/media/INTER-REGULAR.59e981f7.TTF"
+  },
+  {
+    "revision": "4b0030232a90b8ed4afa879b2a69a62f",
+    "url": "/medicine-consultation-client/static/media/INTER-SEMIBOLD.4b003023.TTF"
+  },
+  {
+    "revision": "9935ef7721d0efdd557ceaa0bbaf739b",
+    "url": "/medicine-consultation-client/static/media/login-bg.9935ef77.png"
+  },
+  {
+    "revision": "48b263a328c62cddde709cf42fb1dda6",
+    "url": "/medicine-consultation-client/static/media/logo.48b263a3.svg"
+  },
+  {
+    "revision": "c80d2b3018ba6eafe8651e531b44cefc",
+    "url": "/medicine-consultation-client/static/media/noData.c80d2b30.png"
+  },
+  {
+    "revision": "6c55796b331f7124a587c08a75b0ea6c",
+    "url": "/medicine-consultation-client/static/media/requests.6c55796b.svg"
+  },
+  {
+    "revision": "5891a0b6d796b12c9a928ab9ebae3bd6",
+    "url": "/medicine-consultation-client/static/media/signup-bg.5891a0b6.png"
+  }
+]);
