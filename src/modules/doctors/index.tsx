ccc83c8f7268ad2c7280a0032c7330a0800286b4
Tandem-web <LebedevWebProgrammer@yandex.ru>
2020-09-06 22:15:22 +0300

    сonnect symptoms page
    сonnect the symptoms page to the project

--- src/modules/doctors/index.tsx
+++ src/modules/doctors/index.tsx
@@ -1,10 +1,12 @@
 import ChooseDoctor from "./pages/find-doctor";
 import DetailDoctorPage from './pages/detail';
 import FilterCityModal from './components/filter/city-add-modal';
+import SymptomsPage from './pages/symptoms';
 
 
 export {
     ChooseDoctor,
     FilterCityModal,
     DetailDoctorPage,
+    SymptomsPage,
 }
---
+++ src/modules/doctors/pages/symptoms.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+const SymptomsPage: React.FC = () => {
+    return <h1>Hello World</h1>
+}
+
+export default SymptomsPage;
