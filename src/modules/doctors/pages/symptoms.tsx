9494ba90cbb4557e645fd67367a92472d7e1a0e6
pandacode <the1ime@yandex.ru>
2020-09-10 21:53:57 +0500

    /symptoms: comment symptoms tabs import

--- src/modules/doctors/pages/symptoms.tsx
+++ src/modules/doctors/pages/symptoms.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import SymptomsWrap from '../components/symptoms-title';
 import Title from "../components/symptoms-title";
-import SymptomsTabs from '../components/symptoms-tabs'
+// import SymptomsTabs from '../components/symptoms-tabs'
 import ConfirmButton from '../../../components/confirm-button';
 const SymptomsPage: React.FC = () => {
     return (
         <>
             <div className="symptoms-wrapper">
                 <Title />
-                <div className="symptoms-main-container">
+                {/* <div className="symptoms-main-container">
                     <SymptomsTabs />
-                </div>
+                </div> */}
                 <div className='symptoms-but'>
-                   <ConfirmButton content='Продолжить' onConfirm={()=>{console.log(1)}}/>
+                    <ConfirmButton content='Продолжить' onConfirm={() => { console.log(1) }} />
                 </div>
             </div>
         </>
