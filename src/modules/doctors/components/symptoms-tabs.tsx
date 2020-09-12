8ae39b608b93101a3ef49a2e0e1498aa6060a7c8
pandacode <the1ime@yandex.ru>
2020-09-12 20:23:23 +0500

    /symptoms: fix build errors

--- src/modules/doctors/components/symptoms-tabs.tsx
+++ src/modules/doctors/components/symptoms-tabs.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 const SymptomsTabs: React.FC = () => {
-	type eType = object|string|number|any;
+	type eType = object | string | number | any;
 	type TabsType = {
-		items:any
+		items: any
 	}
-	let items: {title: string, sourseSvg: string, active: boolean, id: number}[] = [
-		{ title: 'М', sourseSvg: '/' , active: true, id: 0},
-		{ title: 'Ж', sourseSvg: '/' , active: false, id: 1},
+	let items: { title: string, sourseSvg: string, active: boolean, id: number }[] = [
+		{ title: 'М', sourseSvg: '/', active: true, id: 0 },
+		{ title: 'Ж', sourseSvg: '/', active: false, id: 1 },
 	];
 	// {items[active] && <TabContent {...items[active]} />}	
-	const Tabs:React.FC<TabsType> = ({ items }:any ) => {
-		const TabContent:React.FC = ({title, sourseSvg}:any) => {
-			return(
+	const Tabs: React.FC<TabsType> = ({ items }: any) => {
+		const TabContent: React.FC = ({ title, sourseSvg }: any) => {
+			return (
 				<div className="tab-content">
 					<div className="tab-content-img-wrap">
 						<div className='fake-img img-1'>
@@ -20,32 +20,32 @@ const SymptomsTabs: React.FC = () => {
 						</div>
 						<div className='fake-img img-2'>
 
-						</div>		
-					</div>	
+						</div>
+					</div>
 				</div>
 			)
 		};
-		const [ active, setActive ] = React.useState(items);
+		const [active, setActive] = React.useState(items);
 
-		// function openTab(e:any){
-		// 	e.persist()
-		// 	let id = +e._targetInst.key;
-		// 	return items = items.map((item:{}) => {
-		// 		(item.id == id) ? !item.active : 
-		// 	})
-		// 	console.log(e)
-		// };
+		function openTab(e: any) {
+			// e.persist()
+			// let id = +e._targetInst.key;
+			// return items = items.map((item:{}) => {
+			// 	(item.id == id) ? !item.active : 
+			// })
+			// console.log(e)
+		};
 		return (
 			<div className='tabs-wrapper'>
 				<div className='tabs-ui-container'>
 					{console.log(items[0].active)}
 					<TabContent />
 					<div className="symptoms-tab">
-						{items.map((n:any, i:any) => (
+						{items.map((n: any, i: any) => (
 							<button key={n.id}
-							className={`tab-links tab-${n.id} ${n.active === true ? 'tab-active' : ''}`}
-							onClick={openTab}
-							data-index={i}
+								className={`tab-links tab-${n.id} ${n.active === true ? 'tab-active' : ''}`}
+								onClick={openTab}
+								data-index={i}
 							>{n.title}</button>
 						))}
 					</div>
@@ -53,7 +53,7 @@ const SymptomsTabs: React.FC = () => {
 			</div>
 		);
 	}
-	return <Tabs items={items}/>;
+	return <Tabs items={items} />;
 
 }
 export default SymptomsTabs;
