b3ca2a9611154b11a131dcb9a52aa2fcca2702e2
pandacode <the1ime@yandex.ru>
2020-11-02 12:18:24 +0500

    rewrite basic information

--- src/modules/doctors/pages/detail.tsx
+++ src/modules/doctors/pages/detail.tsx
@@ -41,64 +41,22 @@ const DetailPage: React.FC<Props> = (props) => {
                 : <React.Fragment>
                     <header>
 
-                        {
-                            controller.loading ? <Skeleton width={profileImageSkeletonWidth} height={profileImageSkeletonWidth} /> : <div className="profile-image" style={profileStyles} />
-                        }
+                        <div className="profileImage" style={profileStyles}/>
 
-
-                        <div className="information">
-                            <h1>
-                                {
-                                    !controller.loading
-                                        ? <React.Fragment>
-                                            {controller.doctor?.name + " " + controller.doctor?.surname + " " + controller.doctor?.patronymic}
-                                            <BookmarkIcon booked={false} className="bookmark" />
-                                        </React.Fragment>
-                                        : <Skeleton width={nameSkeletonWidth} height={30} />
-                                }
-
-
-                            </h1>
-
-                            <ul className="information-list">
-                                {
-                                    controller.loading
-                                        ? <Skeleton style={{ display: "flex", flexDirection: "column", margin: "10px 0" }} count={3} width={300} />
-
-                                        : <React.Fragment>
-                                            <li>
-                                                <span className="name">Специальность:</span>
-                                                <span className="data">{controller.doctor?.speciality}</span>
-                                            </li>
-                                            <li>
-                                                <span className="name">Стаж работы:</span>
-                                                <span className="data">{controller.formatExperience(controller.doctor?.experience ?? 0)}</span>
-                                            </li>
-                                            <li>
-                                                <span className="name">Пол:</span>
-                                                <span className="data">{controller.doctor?.sex ? "Мужской" : "Женский"}</span>
-                                            </li>
-                                            <li>
-                                                <span className="name">Возраст:</span>
-                                                <span className="data">{controller.doctor?.age + " " + controller.declOfNum(controller.doctor?.age ?? 0, ['год', 'года', 'лет'])}</span>
-                                            </li>
-                                            {controller.doctor?.city
-                                                ? <li>
-                                                    <span className="name">Город:</span>
-                                                    <span className="data">{controller.doctor?.city}</span>
-                                                </li>
-                                                : <React.Fragment />
-                                            }
-                                        </React.Fragment>
-                                }
-                            </ul>
-
-                            <RatingComponent amount={controller.doctor?.rating ?? 0} />
+                        <div className="info-main">
+                            <h2>Иванов Иван</h2>
+                            <span id="speciality">Врач Терапевт</span>
+                            <RatingComponent amount={4.5}/>
+                            <div className="buttons">
+                                <button className="appoint">Записаться</button>
+                                {/* todo: save logic */}
+                                <button className="save" ><BookmarkIcon/></button>
+                            </div>
                         </div>
-                        {
-                            controller.loading ? <Skeleton style={buttonSkeletonStyles} width={buttonSkeletonWidth} height={35} /> : <button id="consultation-signup">Записаться</button>
-                        }
+
                     </header>
+
+
                     <ConsultationSelector />
                     {
 
