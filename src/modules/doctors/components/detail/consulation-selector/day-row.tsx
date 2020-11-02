5a1f3bfda9d2dd4be2d6d1119b823fc9f807b6d9
pandacode <the1ime@yandex.ru>
2020-11-02 23:29:53 +0500

    redesign day ui

--- src/modules/doctors/components/detail/consulation-selector/day-row.tsx
+++ src/modules/doctors/components/detail/consulation-selector/day-row.tsx
@@ -9,29 +9,21 @@ type Props = {
 
 const DayRow: React.FC<Props> = (props: Props) => {
 
-    // console.log(window.innerWidth);
-    // console.log(controller.getUIDayMarker([1, 2, 3, 4, 6, 7, 8]));
+    if (props.isWeekend) return <React.Fragment/>
 
     return <div className="day-row" id={props.day}>
         <div className="day-name">{props.day}</div>
+        <div className="times">
+            {
+                controller.getUIDayMarker([1, 2, 3, 4, 6, 7, 8]).map((e, i) => {
+                    let className = "time x" + e.x;
+                    if (e.isOccupied) className += " occupied ";
 
-        {
-            props.isWeekend
-                ? <div className="weekend">Выходной</div>
-                : <div className="times">
-                    {
-                        controller.getUIDayMarker([1, 2, 3, 4, 6, 7, 8]).map((e, i) => {
-                            let className = "time x" + e.x;
-                            if (e.isOccupied) className += " occupied ";
+                    return <div className={className} key={`time-${i}`}>{e.title}</div>
+                })
+            }
 
-                            return <div className={className} key={`time-${i}`}>{e.title}</div>
-                        })
-                    }
-
-                </div>
-        }
-
-
+        </div>
     </div >
 }
 
