567a4637d421878e6a54d4474f57eb9e95d9747a
pandacode <the1ime@yandex.ru>
2020-11-14 17:55:15 +0500

    connect notification logic

--- src/modules/settings/pages/notification.tsx
+++ src/modules/settings/pages/notification.tsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { observer } from "mobx-react";
+
+import controller from "../controllers/notifications-controller";
 import Navigation from "../components/navigation";
 import TextField from "../../../components/text-field";
 import Checkbox from "../../../components/checkbox";
 import ConfirmButton from "../../../components/confirm-button"
+import LoadingLine from "../../../components/loading-line";
+import SettingsLoadingComponent from "../components/loading";
 
 const NotificationPage : React.FC = () => {
 
+    useEffect(() => {
+        controller.fetchNotifications();
+    }, []);
+
+    if (controller.isLoading) {
+        return <SettingsLoadingComponent active={3}/>
+    }
     return <main className="notifications-page settings-page">
+
+        {
+            controller.isLoadingSave ? <LoadingLine/> : <React.Fragment/>
+        }
+
         <Navigation active={3} />
         <section className="content notifications">
             <TextField
-                onChange={(v) => {}}
+                onChange={(v) => controller.email = v}
+                value={controller.email}
                 field="Email для уведомлений"
-                needErrorHandle={false}
+                error={controller.emailError}
             />
-            <Checkbox onChange={() => {}} label="Присылать уведомления о консультациях" checked={false} />
-            <Checkbox onChange={() => {}} label="Рассылка по электронной почте" checked={true} />
-            <ConfirmButton onConfirm={() => {}} content="Сохранить" />
+
+            <Checkbox
+                onChange={() => controller.needNotifications = !controller.needNotifications}
+                label="Присылать уведомления о консультациях"
+                checked={controller.needNotifications} />
+            <Checkbox
+                onChange={() => controller.canDoMailing = !controller.canDoMailing}
+                label="Рассылка по электронной почте"
+                checked={controller.canDoMailing}/>
+
+            <ConfirmButton onConfirm={controller.saveAccountSettings} content="Сохранить" />
         </section>
     </main>
 
 }
 
-export default NotificationPage;
+export default observer(NotificationPage);
