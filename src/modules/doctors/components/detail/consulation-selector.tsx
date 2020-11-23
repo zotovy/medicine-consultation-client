d1e3367b92248112b3a944b6b696849f00036dc0
pandacode <the1ime@yandex.ru>
2020-11-23 11:05:34 +0500

    create skeletons for new header at /detail page

--- src/modules/doctors/components/detail/consulation-selector.tsx
+++ src/modules/doctors/components/detail/consulation-selector.tsx
@@ -8,36 +8,19 @@ import Title from "../../../../components/title";
 
 
 const ConsultationSelector: React.FC = () => {
-
-    // Skeletons
-    const titleSkeletonStyles: object = window.screen.width < 640 ? { marginLeft: "calc((100vw - 162px) / 2)" } : {}
-    const weekSelectorSkeletonWidth: number = window.screen.width < 640 ? window.screen.width - 32 : 150;
-
     return <div className="selector">
-        {controller.loading ? <Skeleton width={130} style={titleSkeletonStyles} height={24} className="title" /> : /*<Title title="Консультация" start/>*/ null}
-        {
-            controller.loading
-                ? <Skeleton width={weekSelectorSkeletonWidth} height={35} className="week-selector" />
-                : <div className="week-selector">
-                    <Chevron open={false} id="left" onClick={controller.previousWeek} />
-                    <span id="data">{controller.getFormattedFromDate()} - {controller.getFormattedToDate()}</span>
-                    <Chevron open={false} id="right" onClick={controller.nextWeek} />
-                </div>
-        }
-        {
-            controller.loading
-                ? <Skeleton count={7} height={28} style={{ margin: "3px 0" }} />
-                : <React.Fragment>
-                    <DayRow day="Пн" occupied={[1, 2, 3]} isWeekend={false} />
-                    <DayRow day="Вт" occupied={[1, 2, 3]} isWeekend={false} />
-                    <DayRow day="Ср" occupied={[1, 2, 3]} isWeekend={false} />
-                    <DayRow day="Чт" occupied={[1, 2, 3]} isWeekend={false} />
-                    <DayRow day="Пт" occupied={[1, 2, 3]} isWeekend={false} />
-                    <DayRow day="Сб" occupied={[1, 2, 3]} isWeekend={false} />
-                    <DayRow day="Вс" occupied={[1, 2, 3]} isWeekend={true} />
-                </React.Fragment>
-        }
-
+        <div className="week-selector">
+            <Chevron open={false} id="left" onClick={controller.previousWeek}/>
+            <span id="data">{controller.getFormattedFromDate()} - {controller.getFormattedToDate()}</span>
+            <Chevron open={false} id="right" onClick={controller.nextWeek}/>
+        </div>
+        <DayRow day="Пн" occupied={[1, 2, 3]} isWeekend={false}/>
+        <DayRow day="Вт" occupied={[1, 2, 3]} isWeekend={false}/>
+        <DayRow day="Ср" occupied={[1, 2, 3]} isWeekend={false}/>
+        <DayRow day="Чт" occupied={[1, 2, 3]} isWeekend={false}/>
+        <DayRow day="Пт" occupied={[1, 2, 3]} isWeekend={false}/>
+        <DayRow day="Сб" occupied={[1, 2, 3]} isWeekend={false}/>
+        <DayRow day="Вс" occupied={[1, 2, 3]} isWeekend={true}/>
     </div>
 }
 
--- src/modules/doctors/pages/detail.tsx
+++ src/modules/doctors/pages/detail.tsx
@@ -3,12 +3,14 @@ import { withRouter, RouteComponentProps } from "react-router-dom";
 import Skeleton from 'react-loading-skeleton';
 import { observer } from "mobx-react";
 import controller from "../controllers/detail-controller";
-import {BookmarkIcon, InstagramIcon, MailIcon, TelegramIcon, ViberIcon, VkIcon, WhatsAppIcon} from "../icons";
+import { BookmarkIcon, InstagramIcon, MailIcon, TelegramIcon, ViberIcon, VkIcon, WhatsAppIcon } from "../icons";
 import RatingComponent from "../components/rating";
 import ConsultationSelector from "../components/detail/consulation-selector";
 import Reviews from "../components/detail/reviews";
 import DoctorPlaceholder from "../../../static/images/user-placeholder.jpg";
 import Page404 from "../../main/pages/404";
+import LoadingHeader from "../components/detail/loading/loading-header";
+import LoadingConsultationsComponent from "../components/detail/loading/loading-consultations";
 
 type PathParamsType = {
     id: string,
@@ -27,30 +29,40 @@ const DetailPage: React.FC<Props> = (props) => {
         backgroundImage: `url(${controller.doctor?.photoUrl || DoctorPlaceholder})`,
     };
 
-    // Skeleton
-    const profileImageSkeletonWidth: number = window.screen.width < 640 ? window.screen.width - 32 : 175;
-    const nameSkeletonWidth: number = window.screen.width < 640 ? window.screen.width - 32 : 350;
-    const buttonSkeletonWidth: number = window.screen.width < 640 ? window.screen.width - 32 : 135
-    const buttonSkeletonStyles: object = window.screen.width < 640 ? { marginTop: "10px" } : {}
+    if (controller.loading) {
+        return <div className="detail-doctor-module">
+            <LoadingHeader/>
+            <LoadingConsultationsComponent/>
+        </div>;
+    }
 
     return <div className="detail-doctor-module">
         {
 
             !controller.loading && controller.doctor === undefined
-                ? <Page404 />
+                ? <Page404/>
                 : <React.Fragment>
                     <header>
 
                         <div className="profileImage" style={profileStyles}/>
 
                         <div className="info-main">
-                            <h2>Иванов Иван</h2>
-                            <span id="speciality">Врач Терапевт</span>
+                            <h2> {controller.doctor?.fullName} </h2>
+
+                            <span id="speciality">
+                                {
+                                    controller.doctor?.speciality
+                                        ? controller.doctor.speciality.length > 0
+                                        ? controller.doctor.speciality[0]
+                                        : ""
+                                        : ""
+                                }
+                            </span>
                             <RatingComponent amount={4.5}/>
                             <div className="buttons">
                                 <button className="appoint">Записаться</button>
                                 {/* todo: save logic */}
-                                <button className="save" ><BookmarkIcon/></button>
+                                <button className="save"><BookmarkIcon/></button>
                             </div>
                         </div>
 
@@ -84,12 +96,12 @@ const DetailPage: React.FC<Props> = (props) => {
                     </header>
 
 
-                    <ConsultationSelector />
+                    <ConsultationSelector/>
                     {
 
                         controller.doctor?.clientsReviews?.length ?? 0 > 0
-                            ? <Reviews />
-                            : <React.Fragment />
+                            ? <Reviews/>
+                            : <React.Fragment/>
                     }
                 </React.Fragment>
         }
