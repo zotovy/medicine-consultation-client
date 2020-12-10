83a1ea3735ecc239f412ed97c45ceeb65f331e80
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2020-12-10 18:47:37 +0500

    /doctor: create image tile & finish additional information section

--- src/modules/doctors/components/detail/additional-information/index.tsx
+++ src/modules/doctors/components/detail/additional-information/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import controller from "../../../controllers/detail-controller";
 import { observer } from "mobx-react";
 import TileComponent from "./tile";
+import ImageTile from "./image-tile";
 
 // Tab classes
 const underlineClasses = ["first", "second", "third"];
@@ -32,18 +33,25 @@ const Index : React.FC = () => {
                     years="2005 - 2016"
                     title="ООО «Клиника классической медицины»"
                     subtitle="Невролог, мануальный терапевт"/>
+                <TileComponent
+                    years="2016 - 2018"
+                    title="ООО «Клиника Нейроортопедии»"
+                    subtitle="Невролог, остеопат, мануальный терапевт"/>
+                <TileComponent
+                    years="с 2018"
+                    title="ООО «Дискотерапия плюс»"
+                    subtitle="Невролог, остеопат, мануальный терапевт"/>
             </div>
             <div className="content_page content_page-2">
                 <TileComponent
-                    years="2005 - 2016"
-                    title="ООО «Клиника классической медицины»"
-                    subtitle="Невролог, мануальный терапевт"/>
+                    years="1981"
+                    title="Пермский государственный медицинский университет им. акад. Е.А. Вагнера (лечебное дело)"
+                    subtitle="Базовое образование"/>
             </div>
             <div className="content_page content_page-3">
-                <TileComponent
-                    years="2005 - 2016"
-                    title="ООО «Клиника классической медицины»"
-                    subtitle="Невролог, мануальный терапевт"/>
+                <ImageTile
+                    title="Диплом по специальности «Лечебное дело"
+                    imageUrl="https://ortho-rus.ru/uploads/posts/2019-09/1569532135_1-e1512037094751.jpg" />
             </div>
         </div>
     </section>
