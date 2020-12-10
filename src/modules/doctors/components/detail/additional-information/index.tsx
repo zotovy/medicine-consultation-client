8fbe75924834895032060c2f4eb5c2a0a42505f5
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2020-12-10 18:09:25 +0500

    create tile component & slider

--- src/modules/doctors/components/detail/additional-information/index.tsx
+++ src/modules/doctors/components/detail/additional-information/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import controller from "../../../controllers/detail-controller";
 import { observer } from "mobx-react";
+import TileComponent from "./tile";
 
 // Tab classes
 const underlineClasses = ["first", "second", "third"];
 const getTabClass = (i: number) => `tab ${controller.selectedTabIndex == i ? "selected" : ""}`;
 const handleClick = (i: number) => controller.selectedTabIndex = i;
+const getSelectedClass = () => `selected${controller.selectedTabIndex + 1}`;
 
 const Index : React.FC = () => {
 
@@ -23,7 +25,27 @@ const Index : React.FC = () => {
             <div className={`underline ${underlineClasses[controller.selectedTabIndex]}`}/>
         </div>
 
-
+        {/* ------- CONTENT ------- */}
+        <div className={`content  ${getSelectedClass()}`}>
+            <div className="content_page content_page-1">
+                <TileComponent
+                    years="2005 - 2016"
+                    title="ООО «Клиника классической медицины»"
+                    subtitle="Невролог, мануальный терапевт"/>
+            </div>
+            <div className="content_page content_page-2">
+                <TileComponent
+                    years="2005 - 2016"
+                    title="ООО «Клиника классической медицины»"
+                    subtitle="Невролог, мануальный терапевт"/>
+            </div>
+            <div className="content_page content_page-3">
+                <TileComponent
+                    years="2005 - 2016"
+                    title="ООО «Клиника классической медицины»"
+                    subtitle="Невролог, мануальный терапевт"/>
+            </div>
+        </div>
     </section>
 }
 
