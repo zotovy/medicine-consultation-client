09a8dc599d4137672e79e8bbe3a99e6a79ad83c2
Tandem-web <LebedevWebProgrammer@yandex.ru>
2020-12-21 14:42:05 +0300

    create chat animation for "How it use" section

--- src/modules/main/controller.ts
+++ src/modules/main/controller.ts
@@ -1,20 +1,21 @@
-import { observable, action } from "mobx";
-import axios from "axios";
-import { toJS } from "mobx";
+import { action } from "mobx";
 
 class AnimController {
     @action startAnim = () => {
-        const animItems: any = document.querySelectorAll("._anim-items");
-        console.log(animItems)
+        const   animItems: any = document.querySelectorAll("._anim-items"),
+                msg1: Element | any = document.querySelector(".message_container.message_container__left"),
+                msgLoader1: Element | any = document.querySelector(".message__block-sending.message__block-sending"),
+                msgCheckIcon1: Element | any = document.querySelector(".read-indicator-1"),
+                msg2: Element | any = document.querySelector(".message_container.message_container__right"),
+                msgLoader2: Element | any = document.querySelector(".message__block-sending.message__block-sending-right"),
+                msgCheckIcon2: Element | any = document.querySelector(".read-indicator-2");
         if (animItems.length > 0) {
-            console.log(1)
-
             function onScroll() {
                 for (let i = 0; i < animItems.length; i++) {
-                    const animItem = animItems[i];
-                    const animItemHeight = animItem.offsetHeight;
-                    const animItemOffset = offset(animItem).top;
-                    const animStart = 4;
+                    const   animItem = animItems[i],
+                            animItemHeight = animItem.offsetHeight,
+                            animItemOffset = offset(animItem).top,
+                            animStart = 4;
 
                     let animItemPoint = window.innerHeight - animItemHeight / 4;
                     if (animItemHeight > window.innerHeight) {
@@ -23,6 +24,7 @@ class AnimController {
 
                     if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
                         animItem.classList.add("_show-elem")
+                        chatAnimation(animItem)
                     } else {
                         if (!animItem.classList.contains('_anim-not-hide')) {
                             animItem.classList.remove("_show-elem")
@@ -44,8 +46,33 @@ class AnimController {
                 onScroll()
             }, 300)
 
-            function chatAnimation() {
-
+            function chatAnimation(animItem: Element) {
+                if(animItem.classList.contains("messenger")&&animItem.classList.contains("_show-elem")&&!animItem.classList.contains("_stopChat")){
+                    animItem.classList.add("_stopChat");
+                    setTimeout(() => {
+                        msg1.classList.remove("message-hidden");
+                        msg1.classList.add("slide-in-left");
+                        msgLoader1.classList.add("message-hidden");
+                        setTimeout(() => {
+                            msgCheckIcon1.classList.remove("read-indicator-hidden");
+                            msgCheckIcon1.classList.add("draw-line");
+                            setTimeout(() => {
+                                msgLoader2.classList.add("message-hidden");
+                                msgLoader2.classList.remove("message-hidden");
+                                msgLoader2.classList.add("slide-in-right");
+                                setTimeout(() => {
+                                    msg2.classList.remove("message-hidden");
+                                    msg2.classList.add("slide-in-right")
+                                    msgLoader2.classList.add("message-hidden");
+                                    setTimeout(() => {
+                                        msgCheckIcon2.classList.remove("read-indicator-hidden"); 
+                                        msgCheckIcon2.classList.add("draw-line");
+                                    },1000)
+                                },2000)     
+                            },250)
+                        },1000)
+                    },3000)
+                }
             }
         }
     }
