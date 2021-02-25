96c3a0fa0301ae2096d085a74c94a4ca4cdfa5c4
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-02-25 16:37:25 +0500

    Update detail-information-mobile.tsx

--- modules/doctors/components/detail/detail-information-mobile.tsx
+++ modules/doctors/components/detail/detail-information-mobile.tsx
@@ -15,71 +15,71 @@ const getClasses = (name: string, length: number | undefined) => {
 
 
 const DetailInformationMobile: React.FC = () => {
-    return <React.Fragment/> // todo: fix .toString() of undefined
-    const controller = useInjection<DetailController>(TYPES.detailDoctorController);
-    if (!controller.doctor) return <React.Fragment/>;
-
-    return <div className={`detail-information-mobile ${controller.isMobileInformationPageOpen ? "" : "closed"}`}>
-            <div className="close" onClick={() => controller.isMobileInformationPageOpen = false}><CloseIcon/></div>
-            <h1>Информация</h1>
-            <div className="information">
-                <div className="value">
-                    Специальность:&nbsp;
-                    {
-                        controller.doctor.speciality.length > 0
-                            ? formatServices.translateSpeciality(controller.doctor.speciality[0])
-                            : "Не указана"
-                    }
-                </div>
-                <div className="value">
-                    Опыт работы:&nbsp;
-                    {
-                        controller.doctor.experience
-                            ? formatServices.experience(controller.doctor.experience)
-                            : "Не указан"
-                    }
-                </div>
-                <div className="value">
-                    Возраст:&nbsp;
-                    {
-                        controller.doctor.age
-                            ? formatServices.age(controller.doctor.age)
-                            : "Не указан"
-                    }
-                </div>
-                <div className="value">
-                    Город:&nbsp;
-                    {
-                        controller.doctor.city && controller.doctor.city.length > 0
-                            ? controller.doctor.city
-                            : "Не указан"
-                    }
-                </div>
-            </div>
-
-            <section className={getClasses("work-places", controller.doctor.workPlaces?.length)}>
-                <h3 className="title">Опыт работы</h3>
-                {
-                    DoctorDetailHelper.getWorkPlaces(controller.doctor.workPlaces).map(e => <TileComponent key={`workplace-${e.title}`} {...e} />)
-                }
-            </section>
-
-            <section className={getClasses("education", controller.doctor.education?.length)}>
-                <h3 className="title">Образование</h3>
-                {
-                    DoctorDetailHelper.getEducation(controller.doctor.education).map(e => <TileComponent key={`education-${e.title}`} {...e} />)
-                }
-            </section>
-
-            <section className={getClasses("qualification-proofs", controller.doctor.qualificationProofs?.length)}>
-                <h3 className="title">Квалификация</h3>
-                {
-                    controller.doctor.qualificationProofs?.map(e => <ImageTileComponent key={`qualification-${e.name}`} title={e.name} imageUrl={e.imageUrl}/>)
-                    ?? <React.Fragment/>
-                }
-            </section>
-
-    </div>
+    return <React.Fragment/>; // todo: fix .toString() of undefined
+    // const controller = useInjection<DetailController>(TYPES.detailDoctorController);
+    // if (!controller.doctor) return <React.Fragment/>;
+    //
+    // return <div className={`detail-information-mobile ${controller.isMobileInformationPageOpen ? "" : "closed"}`}>
+    //         <div className="close" onClick={() => controller.isMobileInformationPageOpen = false}><CloseIcon/></div>
+    //         <h1>Информация</h1>
+    //         <div className="information">
+    //             <div className="value">
+    //                 Специальность:&nbsp;
+    //                 {
+    //                     controller.doctor.speciality.length > 0
+    //                         ? formatServices.translateSpeciality(controller.doctor.speciality[0])
+    //                         : "Не указана"
+    //                 }
+    //             </div>
+    //             <div className="value">
+    //                 Опыт работы:&nbsp;
+    //                 {
+    //                     controller.doctor.experience
+    //                         ? formatServices.experience(controller.doctor.experience)
+    //                         : "Не указан"
+    //                 }
+    //             </div>
+    //             <div className="value">
+    //                 Возраст:&nbsp;
+    //                 {
+    //                     controller.doctor.age
+    //                         ? formatServices.age(controller.doctor.age)
+    //                         : "Не указан"
+    //                 }
+    //             </div>
+    //             <div className="value">
+    //                 Город:&nbsp;
+    //                 {
+    //                     controller.doctor.city && controller.doctor.city.length > 0
+    //                         ? controller.doctor.city
+    //                         : "Не указан"
+    //                 }
+    //             </div>
+    //         </div>
+    //
+    //         <section className={getClasses("work-places", controller.doctor.workPlaces?.length)}>
+    //             <h3 className="title">Опыт работы</h3>
+    //             {
+    //                 DoctorDetailHelper.getWorkPlaces(controller.doctor.workPlaces).map(e => <TileComponent key={`workplace-${e.title}`} {...e} />)
+    //             }
+    //         </section>
+    //
+    //         <section className={getClasses("education", controller.doctor.education?.length)}>
+    //             <h3 className="title">Образование</h3>
+    //             {
+    //                 DoctorDetailHelper.getEducation(controller.doctor.education).map(e => <TileComponent key={`education-${e.title}`} {...e} />)
+    //             }
+    //         </section>
+    //
+    //         <section className={getClasses("qualification-proofs", controller.doctor.qualificationProofs?.length)}>
+    //             <h3 className="title">Квалификация</h3>
+    //             {
+    //                 controller.doctor.qualificationProofs?.map(e => <ImageTileComponent key={`qualification-${e.name}`} title={e.name} imageUrl={e.imageUrl}/>)
+    //                 ?? <React.Fragment/>
+    //             }
+    //         </section>
+    //
+    // </div>
 }
 
 export default observer(DetailInformationMobile);
