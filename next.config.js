2d9e8eede523c598dfad5177ea387bd41920eef0
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-02-25 16:25:26 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
-module.exports = {
-    env: {
-        SERVER_URL: "https://localhost:5000",
+const { PHASE_DEVELOPMENT_SERVER, PHASE_EXPORT, PHASE_PRODUCTION_BUILD, PHASE_PRODUCTION_SERVER } = require('next/constants')
+
+
+module.exports = (phase) => {
+
+    console.log(phase);
+
+    if (phase === PHASE_PRODUCTION_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+        return {
+            env: {
+                SERVER_URL: process.env.SERVER_URL,
+            }
+        }
+    }
+
+    return {
+        env: {
+            SERVER_URL: "https://localhost:5000",
+        }
     }
 }
