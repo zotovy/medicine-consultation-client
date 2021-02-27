19970ae7563fc5d8bec537d322c8f704ad5bead7
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-02-27 22:54:52 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
-module.exports = {
-    env: {
-        SERVER_URL: "https://localhost:5000",
-        PEER_SERVER_URL: "localhost",
-        PEER_SERVER_PORT: "5001",
+const { PHASE_DEVELOPMENT_SERVER, PHASE_EXPORT, PHASE_PRODUCTION_BUILD, PHASE_PRODUCTION_SERVER } = require('next/constants')
+
+module.exports = (phase) => {
+
+    console.log(phase);
+
+    if (phase === PHASE_PRODUCTION_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+        return {
+            env: {
+                SERVER_URL: process.env.SERVER_URL,
+                PEER_SERVER_PORT: process.env.PEER_SERVER_URL_PORT,
+                PEER_SERVER_URL: process.env.PEER_SERVER_URL,
+            }
+        }
+    }
+
+    return {
+        env: {
+            SERVER_URL: "https://localhost:5000",
+            PEER_SERVER_PORT: "5001",
+            PEER_SERVER_URL: "localhost",
+        }
     }
 }
+
